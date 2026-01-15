369ea0275f36ece6d45a62566b5a20ae50838716
andywolfe15 <andywolfe15@yahoo.com>
2026-01-15 16:57:54 -0600

    Add generated Supabase types

---
+++ types/supabase.ts
@@ -0,0 +1,16 @@
+[?25l[?2004h
+                                                                                                        
+  >  1. iidsnmsvvjqeafmkgvqz [name: War_Room_Prod_Test(1), org: inqcoptixykmduhajmaj, region: us-east-2]
+    2. zzqpgkdfrcrlimvqdglj [name: War_Room_Prod/Test(2), org: inqcoptixykmduhajmaj, region: us-east-2] 
+    3. sugewjaiokcxeeicccwn [name: JackedandFast, org: qqxvbuldtjwgkewehpfr, region: us-west-2]         
+    4. zutllqkvzizgwgjkvstw [name: superbuilder6000, org: qqxvbuldtjwgkewehpfr, region: us-west-2]      
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+                                                                                                        
+    ↑/k up • ↓/j down • / filter • q quit • ? more                                                      
+                                                                                                        
